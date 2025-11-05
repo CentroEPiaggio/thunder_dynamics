1cf3a0d6f99136eb42f380a5fcd36e792248fd2c
GiorgioSimonini <giorgio.simonini.gs@gmail.com>
2025-11-05 11:03:39 +0000

    control init

---
+++ src/thunder/library/control.h
@@ -0,0 +1,13 @@
+#ifndef CONTROL_H
+#define CONTROL_H
+
+#include "robot.h"
+
+namespace thunder_ns{
+    
+    int compute_control(Robot& robot);
+	
+}
+
+
+#endif
---
+++ src/thunder/src/control.cpp
@@ -0,0 +1,13 @@
+#include "robot.h"
+
+namespace thunder_ns{
+
+    // compute everything
+	int compute_control(Robot& robot){
+		bool ret = true;
+		// put the compute functions for control
+
+		return ret;
+	}
+
+}
