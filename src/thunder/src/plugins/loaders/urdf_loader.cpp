2e90c920b1e31c890b2f17a5488fca8a36f23f83
GiorgioSimonini <giorgio.simonini.gs@gmail.com>
2026-02-14 12:05:05 +0000

    link parameters ok

--- src/thunder/src/plugins/loaders/urdf_loader.cpp
+++ src/thunder/src/plugins/loaders/urdf_loader.cpp
@@ -374,9 +374,9 @@ namespace thunder_ns {
 			for (int i = 0; i < numJoints; ++i) {
 				const auto& b = active_bodies[i];
 				par_DYN_num[STD_PAR_LINK * i + 0] = static_cast<double>(b.mass);
-				par_DYN_num[STD_PAR_LINK * i + 1] = static_cast<double>(b.com(0));
-				par_DYN_num[STD_PAR_LINK * i + 2] = static_cast<double>(b.com(1));
-				par_DYN_num[STD_PAR_LINK * i + 3] = static_cast<double>(b.com(2));
+				par_DYN_num[STD_PAR_LINK * i + 1] = static_cast<double>(b.mass_moment(0)/b.mass);
+				par_DYN_num[STD_PAR_LINK * i + 2] = static_cast<double>(b.mass_moment(1)/b.mass);
+				par_DYN_num[STD_PAR_LINK * i + 3] = static_cast<double>(b.mass_moment(2)/b.mass);
 				par_DYN_num[STD_PAR_LINK * i + 4] = static_cast<double>(b.inertia_at_origin(0, 0)); // Ixx
 				par_DYN_num[STD_PAR_LINK * i + 5] = static_cast<double>(b.inertia_at_origin(0, 1)); // Ixy
 				par_DYN_num[STD_PAR_LINK * i + 6] = static_cast<double>(b.inertia_at_origin(0, 2)); // Ixz
@@ -386,7 +386,7 @@ namespace thunder_ns {
 			}
 
 			robot->add_parameter("par_DYN", casadi::SX::sym("par_DYN", STD_PAR_LINK * numJoints, 1), par_DYN_num, par_DYN_isSymb, "Dynamic parameters", true);
-			robot->add_parameter("par_REG", casadi::SX::sym("par_REG", STD_PAR_LINK * numJoints, 1), std::vector<double>(STD_PAR_LINK * numJoints, 0), par_DYN_isSymb, "Dynamic parameters for regressor", true);
+			robot->add_parameter("par_REG", casadi::SX::sym("par_REG", STD_PAR_LINK * numJoints, 1), std::vector<double>(STD_PAR_LINK * numJoints, 0), {1}, "Dynamic parameters for regressor", true);
 
 			// --- Gravity --- //
 			std::vector<double> gravity_num = {0.0, 0.0, 0.0}; // default no gravity
